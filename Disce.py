bbefe47dc1a9c0f81567a5b09168850e4cd51998
= <Penguinwithwaffle@Gmail.com>
2013-05-27 23:10:08 +0100

    Finised App Engine Tutoiral started to add basic funcinality.
    Signed-off-by: = <Penguinwithwaffle@Gmail.com>

--- Disce.py
+++ Disce.py
@@ -1,102 +1,77 @@
-#!/usr/bin/env python
+import os
+import urllib
 
 from google.appengine.api import users
-from google.appengine.ext import  ndb
+from google.appengine.ext import ndb
 
-import cgi
-import urllib
-import Wordnik
+import jinja2
 import webapp2
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign?%s" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
 
-    <hr>
-
-    <form>Guestbook name:
-      <input value="%s" name="guestbook_name">
-      <input type="submit" value="switch">
-    </form>
-
-    <a href="%s">%s</a>
-
-  </body>
-</html>
-"""
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
 
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
 # However, the write rate should be limited to ~1/second.
+
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    
-    """Constructs a datastore key for a Guestbook entity with guestbook_name."""
-    
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
     return ndb.Key('Guestbook', guestbook_name)
 
+class Disce(webapp2.RequestHandler):
+    
+    def get(self):
+        
+        disce_template = {
+            
+            
+        }
+        template = JINJA_ENVIRONMENT.get_template('disce.html')
+        self.response.write(template.render(disce_values))
+
+
 class Greeting(ndb.Model):
-    """ Models an individual Guestbook entry with author, content, date."""
+    """Models an individual Guestbook entry with author, content, and date."""
     author = ndb.UserProperty()
-    
     content = ndb.StringProperty(indexed=False)
-    
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainPage(webapp2.RequestHandler):
-    
+
     def get(self):
-        self.response.write('<html><body>')
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
         
-        guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME) 
-                                          
-        
-        # Ancestor Queries, as shown here, are strongly consistent with the High
-        # Replication Datastore. Queries that span entity groups are eventually
-        # consistent. If we omitted the ancestor from this query there would be
-        # a slight chance that Greeting that had just been written would not
-        # show up in a query.
         greetings_query = Greeting.query(
             ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         
         greetings = greetings_query.fetch(10)
-        
-        for greeting in greetings:
-            
-            if greeting.author:
-                
-                self.response.write(
-                    '<b>%s</b> wrote: ' % greeting.author.nickname())
-                
-            else:
-                
-                self.reponse.write('An anonymous person wrote:')
-                
-            self.response.write('<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
-        
+
         if users.get_current_user():
-            
             url = users.create_logout_url(self.request.uri)
-            
-            url_linktext = "Logout"
-            
+            url_linktext = 'Logout'
         else:
-            
             url = users.create_login_url(self.request.uri)
-            
             url_linktext = 'Login'
-            
-        # Write the submission form and the fotter of the page
-        sign_query_params = urllib.urlencode({'guestbook_name': guestbook_name})
-        
-        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE % (sign_query_params, cgi.escape(guestbook_name), url, url_linktext))
-        
-class GuestBook(webapp2.RequestHandler):
-    
+
+        template_values = {
+            'greetings': greetings,
+            'guestbook_name': guestbook_name,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+class Guestbook(webapp2.RequestHandler):
+
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each greeting
         # is in the same entity group. Queries across the single entity group
@@ -104,22 +79,19 @@ class GuestBook(webapp2.RequestHandler):
         # should be limited to ~1/second.
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
-        
         greeting = Greeting(parent=guestbook_key(guestbook_name))
-        
+
         if users.get_current_user():
-            
             greeting.author = users.get_current_user()
-            
+
         greeting.content = self.request.get('content')
-        
         greeting.put()
-        
+
         query_params = {'guestbook_name': guestbook_name}
-        
         self.redirect('/?' + urllib.urlencode(query_params))
-    
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', GuestBook)
+    ('/sign', Guestbook),
+    ('/disce', Disce)
 ], debug=True)
