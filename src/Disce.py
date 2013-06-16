1597eeea74534e862eb9aad0f0af0f32366e49fc
Willifme <william@william-desktop.(none)>
2013-06-16 12:02:25 +0100

    Changed things

--- src/Disce.py
+++ src/Disce.py
@@ -1,8 +1,6 @@
 import os
-import urllib
 
 from google.appengine.api import users
-from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
@@ -12,21 +10,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
-DEFAULT_SEARCH_NAME = 'searchterm_name'
-
-# We set a parent key on the 'Greetings' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
-
-def searchterm_key(searchterm_name=DEFAULT_SEARCH_NAME):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return ndb.Key('Search', searchterm_name)
-
-class Search(ndb.Model):
-
-    """Models an individual Guestbook entry with author, content, and date."""
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
+url_linktext = 'test'
 
 class MainPage(webapp2.RequestHandler):
 
@@ -38,14 +22,13 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(mainmenu_values))
         user = users.get_current_user()
 
-        if user:
-            self.redirect('/disce')
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+  #      if user:
+         #   self.redirect('/disce')
+   #     else:
+    #        self.redirect(users.create_login_url(self.request.uri))
 
 class Disce(webapp2.RequestHandler):
 
-   # test = getDefinition(self).searchQuery
 
     def get(self):
 
@@ -59,9 +42,11 @@ class Disce(webapp2.RequestHandler):
 
         self.response.headers['Content-Type'] = 'text/plain'
 
-        self.response.write('Take reference at www.wordnik.com for proper spelling.Wordnik has a weird captial thing going on.\n')
+        test = "test"
 
-        search = Search(parent=searchterm_key('searchterm_name'))
+        self.response.write('Take reference at www.wordnik.com for proper spelling.Wordnik has a weird captial thing going on.\n you should not be able to see this!')
+
+        self.redirect('/disce/results')
 
         searchQuery = self.request.get('searchQuery')
 
@@ -70,17 +55,13 @@ class Disce(webapp2.RequestHandler):
                                             sourceDictionaries='all',
                                             limit=200)
 
-     #   definitions[0].text
-    test = post()
-
-    print("git test3")
+    test = "test"
 
 class Results(webapp2.RequestHandler):
 
-    def get(self):
-        disce = Disce(webapp2.RequestHandler)
-        url_linktext = disce.test
 
+    def get(self):
+        url_linktext = 'test'
         disce_values = {'url_linktext': url_linktext}
         template = JINJA_ENVIRONMENT.get_template('/resources/www/results.html')
 
