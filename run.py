b3e1607a62c50a1c35c9531ac9002fa5b6a7099c
Willifme <william@william-desktop.(none)>
2013-07-23 19:42:24 +0100

    Disce URL now goes to /results.
    Signed-off-by: Willifme <william@william-desktop.(none)>

--- run.py
+++ run.py
@@ -4,20 +4,27 @@ import sys
 
 sys.path.append('src')  # for importing modules in other folders
 
-from search import Search  # the 'search' module is in the src folder
+from results import Search  # the 'search' module is in the src folder
 
 from changelog import Changelog # the 'changelog' module is in the src folder
 
+from index import Index
+
 app = Flask(__name__)
 
+app.config.from_object('config')
+
 app.add_url_rule('/',
-                view_func=Search.as_view('search'),
-                methods=['GET', 'POST'])
+                 view_func=Index.as_view('index'),
+                 methods=['GET', 'POST'])
 
 app.add_url_rule('/changelog',
                 view_func=Changelog.as_view('changelog'),
                 methods=['GET'])
 
+app.add_url_rule('/results/',
+                view_func=Search.as_view('results'),
+                methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
