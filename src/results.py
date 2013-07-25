ea3d5a8f5c39e6f29f2cfa59a65744c1808a2bc3
Willifme <=>
2013-07-25 16:41:49 +0100

    Added a class for searching Disce directly. re factored some code into the API.
    Signed-off-by: Willifme <=>

--- src/results.py
+++ src/results.py
@@ -9,9 +9,25 @@ sys.path.append('API/python')  # for importing modules in other folders
 
 import disce
 
-class Search(flask.views.MethodView):  # Class for searching
+class DirectURLInput(flask.views.MethodView):
 
-    def post(self, searchQuery): # The page for the results
+    def get(self, searchQuery):
+
+        form = SearchForm()
+
+        return render_template('results.html',
+                                form = form,
+                                searchQuery = disce.getResults(searchQuery),
+                                imageresultsPlace1 = disce.apiList[0],
+                                resultsPlace1 = 'Definition: ' +  disce.apiList[1],
+                                resultsPlace2 = 'Wikipedia article summary: ' + disce.apiList[2],
+                                version = version.version)
+
+class MainResults(flask.views.MethodView):  # Class for searching
+
+    def post(self): # The page for the results
+
+        searchQuery = request.form['searchQuery']
 
         return render_template('results.html',
                                 searchQuery = disce.getResults(searchQuery),
@@ -23,12 +39,19 @@ class Search(flask.views.MethodView):  # Class for searching
 
         form = SearchForm()
 
-        searchQuery = request.args.get('searchQuery')
+        try:
 
-        return render_template('results.html',
-                                form = form,
-                                searchQuery = disce.getResults(searchQuery),
-                                imageresultsPlace1 = disce.apiList[0],
-                                resultsPlace1 = 'Definition: ' +  disce.apiList[1],
-                                resultsPlace2 = 'Wikipedia article summary: ' + disce.apiList[2],
-                                version = version.version)
+            searchQuery = request.args.get('searchQuery')
+
+            return render_template('results.html',
+                                    form = form,
+                                    searchQuery = disce.getResults(searchQuery),
+                                    imageresultsPlace1 = disce.apiList[0],
+                                    resultsPlace1 = 'Definition: ' +  disce.apiList[1],
+                                    resultsPlace2 = 'Wikipedia article summary: ' + disce.apiList[2],
+                                    version = version.version)
+
+        except AttributeError:
+
+            return render_template('search.html',
+                                   form = form)
