3a74bc6b9bb9fd8150c508d79b270f9f681eb6b3
Willifme <=>
2013-07-25 16:37:15 +0100

    The filtering of searchQuery now takes place in the Disce python API.
    Signed-off-by: Willifme <=>

--- src/results.py
+++ src/results.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Request
+from flask import render_template, request
 from forms import SearchForm
 
 import flask.views
@@ -11,49 +11,24 @@ import disce
 
 class Search(flask.views.MethodView):  # Class for searching
 
-    def searchDisce(self,searchQueryURL):
-
-        searchqueryProcessed = searchQueryURL
-
-        searchqueryProcessed = searchqueryProcessed.replace(" ", "%20")
-
-        disce.getResults(searchqueryProcessed)
-
-        searchqueryProcessed = searchqueryProcessed.replace("%20", " ")
-
-        return searchqueryProcessed
-
-    def post(self): # The page for the results
-
-        searchQuery = request.form['searchQuery']
-
-        searchqueryProcessed = self.searchDisce(searchQuery)
-
-        print "test"
+    def post(self, searchQuery): # The page for the results
 
         return render_template('results.html',
-                                searchQuery = searchqueryProcessed,
-                                imageResults =  disce.apiList[0],
+                                searchQuery = disce.getResults(searchQuery),
                                 definitionResults = disce.apiList[1],
                                 wikipediaResults = disce.apiList[2],
                                 version = version.version)
 
     def get(self):
 
-        print "test"
-
         form = SearchForm()
 
         searchQuery = request.args.get('searchQuery')
 
-        searchqueryProcessed = self.searchDisce(searchQuery)
-
-        searchqueryProcessed = searchqueryProcessed.replace("?", "")
-
         return render_template('results.html',
                                 form = form,
-                                searchQuery = searchqueryProcessed,
-                                imageResults =  disce.apiList[0],
-                                definitionResults = disce.apiList[1],
-                                wikipediaResults = disce.apiList[2],
+                                searchQuery = disce.getResults(searchQuery),
+                                imageresultsPlace1 = disce.apiList[0],
+                                resultsPlace1 = 'Definition: ' +  disce.apiList[1],
+                                resultsPlace2 = 'Wikipedia article summary: ' + disce.apiList[2],
                                 version = version.version)
