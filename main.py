dd765cf9272548b34c274f1fea5520b346c549c8
Willifme <=>
2013-08-04 15:45:23 +0100

    No changes made Git thinks I have made changes.
    Signed-off-by: Willifme <=>

---
+++ main.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from flask import Flask
+
+sys.path.append('src')  # for importing modules in other folders
+
+#from search import Search  # the 'search' module is in the src folder
+
+import results
+
+from changelog import Changelog # the 'changelog' module is in the src folder
+
+from index import Index
+
+from homework import Homework
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+@app.route('/', subdomain='egg')
+def test(self):
+    return "test"
+
+app.add_url_rule('/',
+                 view_func=Index.as_view('index'),
+                 methods=['GET', 'POST'])
+
+app.add_url_rule('/changelog',
+                view_func=Changelog.as_view('changelog'),
+                methods=['GET'])
+
+app.add_url_rule('/homework',
+                 view_func=Homework.as_view('homework'),
+                 methods=['GET', 'POST'])
+
+app.add_url_rule('/search/',
+                view_func=results.MainResults.as_view('results'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/search/<searchQuery>',
+                 view_func=results.DirectURLInput.as_view('optionalresults'),
+                 methods=['GET', 'POST'])
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='0.0.0.0')
